2d67100ea95272d91e91a4811293e312773881a6
Дмитрий Черняков <asd.asterix@mail.ru>
2024-07-12 13:51:45 +0700

    Создан список, на главную старницу добавлена возможно добавления данных в список.

---
+++ ItemsAndChanse.cs
@@ -0,0 +1,14 @@
+﻿namespace Calculate
+{
+    public class ItemsAndChanse
+    {
+        public string Items { get; set; }
+        public float Chanse { get; set;}
+
+        public ItemsAndChanse(string items, float chanse)
+        {
+           Items = items; Chanse = chanse;
+        }
+
+    }
+}
